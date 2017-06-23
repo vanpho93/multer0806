3b854d99adc0002bf0ef7217a4af636bf5ec2dde
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-23 09:57:25 +0700

    fileFileter

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const multer = require('multer');
 
+function fileFilter(req, file, cb) {
+    const { mimetype } = file;
+    if (mimetype === 'image/png' || mimetype === 'image/jpeg') {
+        return cb(null, true);
+    }
+    // cb(null, false);
+    cb(new Error('Loi dinh dang file!'));
+}
+
 const storage = multer.diskStorage({
     destination(req, file, cb) {
         cb(null, './public');
     },
     filename(req, file, cb) {
-        cb(null, 'a.png');
+        cb(null, req.body.username + file.originalname);
     }
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage, fileFilter });
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -20,6 +29,9 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
 
-app.post('/upload', upload.single('avatar'), (req, res) => res.send('upload thanh cong'));
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    console.log(req.file);
+    res.send('upload thanh cong');
+});
 
 app.listen(3000, () => console.log('Server started!'));
