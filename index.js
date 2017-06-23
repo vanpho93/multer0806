33620353d1699ea7a4e1bad14616a48ba3e5f614
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-23 10:27:50 +0700

    xong

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 const express = require('express');
-const multer = require('multer');
-
-function fileFilter(req, file, cb) {
-    const { mimetype } = file;
-    if (mimetype === 'image/png' || mimetype === 'image/jpeg') {
-        return cb(null, true);
-    }
-    // cb(null, false);
-    cb(new Error('Loi dinh dang file!'));
-}
-
-const limits = {
-    fileSize: 100 * 1024
-};
-
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, './public');
-    },
-    filename(req, file, cb) {
-        cb(null, req.body.username + file.originalname);
-    }
-});
-
-const upload = multer({ storage, fileFilter, limits }).single('avatar');
+const upload = require('./uploadConfig').single('avatar');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -40,9 +16,5 @@ app.post('/upload', (req, res) => {
         res.send('upload thanh cong');
     });
 });
-
-// app.use((err, req, res, next) => {
-//     res.send('CO LOI');
-// });
 
 app.listen(3000, () => console.log('Server started!'));
